d5fdb1693dbfa4a6d940df2bdb2285fbe4906959
Deeraj <deerajbalamurugan@gmail.com>
2023-12-21 00:05:36 +0530

    updated poststats

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -6,7 +6,32 @@ type PostStatsProps = {
 };
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
-  return <div>PostStats</div>;
+  return (
+    <div className="flex justify-between items-center z-20">
+      <div className="flex gap-2 mr-5">
+        <img
+          src="/assets/icons/like.svg"
+          alt="alike"
+          width={20}
+          height={20}
+          onClick={() => {}}
+          className="cursor-pointer"
+        />
+        <p className="small-medium lg:base-medium">0</p>
+      </div>
+
+      <div className="flex gap-2 ">
+        <img
+          src="/assets/icons/save.svg"
+          alt="save"
+          width={20}
+          height={20}
+          onClick={() => {}}
+          className="cursor-pointer"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default PostStats;
